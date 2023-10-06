1c97f8f1415126d8db3940160a04f6fae5d4badf
NGHIANGUYENVAN\ADMIN <nghianguyenvan@vnpt.vn>
2023-10-06 16:40:11 +0700

    commit

--- D19_DoAn/chart/chart.py
+++ D19_DoAn/chart/chart.py
@@ -4,33 +4,56 @@ import os
 #plot chart
 import plotly.graph_objects as go
 import plotly.express as px
+#matplotlib 
+import matplotlib.pyplot as plt
+from mpl_finance import candlestick_ohlc
+import matplotlib.dates as mpl_dates
 # lay path
-path = ('/').join(os.path.dirname(__file__).split("\\")[:-1])
-data_result = pd.read_csv(path+'/data/data.csv')
+def get_data() :
+    path = ('/').join(os.path.dirname(__file__).split("\\")[:-1])
+    data_result = pd.read_csv(path+'/data/data.csv')
 
 
-data_result = data_result.set_index('Date')
-data_result.drop('Unnamed: 0',axis = 1,inplace = True)
-data_result['Difference'] = [data_result['Close'][i]-data_result['Open'][i] for i in range(len(data_result))]
-data_result = data_result.dropna()
-
-
-print(data_result)
+    # data_result = data_result.set_index('Date')
+    # data_result.drop('Unnamed: 0',axis = 1,inplace = True)
+    data_result['Difference'] = [data_result['Close'][i]-data_result['Open'][i] for i in range(len(data_result))]
+    data_result = data_result.dropna()
+    return data_result
 
 import plotly.graph_objects as go
 import plotly.express as px
+
+
 def candleStick() :
-    fig1 = go.Figure(go.Candlestick(x = data_result.index,
-                                    open = data_result['Open'],
-                                    close = data_result['Close'],
-                                    high = data_result['High'],
-                                    low = data_result['Low']))
-    fig1.show()
+    data_result = get_data()
+    ohlc = data_result.loc[:, ['Date', 'Open', 'High', 'Low', 'Close']]
+    # Converting date into datetime format
+    ohlc['Date'] = pd.to_datetime(ohlc['Date'])
+    ohlc['Date'] = ohlc['Date'].apply(mpl_dates.date2num)
+    ohlc = ohlc.astype(float)
+    # Creating Subplots
+    fig, ax = plt.subplots()
+    candlestick_ohlc(ax, ohlc.values, width=0.6,
+                    colorup='green', colordown='red', alpha=0.8)
+    # Setting labels & titles
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Price')
+    fig.suptitle('Daily Candlestick Chart of NIFTY50')
+    # Formatting Date
+    date_format = mpl_dates.DateFormatter('%d-%m-%Y')
+    ax.xaxis.set_major_formatter(date_format)
+    fig.autofmt_xdate()
+    fig.tight_layout()
+    
+    return plt.gcf()
+
+
 def lineChart() :
+    data_result = get_data()
     fig = px.line(data_result, x=data_result.index, y="Close", title='Close data')
     fig.show()
 
 
 
-candleStick()
-lineChart()
+# candleStick()
+# lineChart()
--- D19_DoAn/main.py
+++ D19_DoAn/main.py
@@ -3,35 +3,60 @@ from kivymd.uix.label import MDLabel
 from kivymd.uix.screen import Screen
 from kivymd.uix.button import MDFlatButton,MDRectangleFlatButton
 from kivymd.uix.textfield import MDTextField
+from kivymd.uix.dialog import MDDialog
 from kivy.lang import Builder
+from kivy.garden.matplotlib import FigureCanvasKivyAgg
+
+from chart.chart import candleStick
 from design.code_helper import code_helper 
-
-
+import pandas as pd
+import os
 
 class MyApp(MDApp) :
     
     def build(self) :
         screen = Screen()
         self.title ="Predict Project"
-
-        btn_search = MDRectangleFlatButton(text = 'Hello World',
-                                  pos_hint = {'center_x': 0.5,'center_y': 0.4},
-                                  on_release = self.get_code)
-        
         self.code = MDTextField(
-                hint_text = "Nhập mã chứng khoán",
-                helper_text_mode = "on_focus",
-                helper_text = "Nhập mã chứng khoán vào đây",
-                pos_hint = {'center_x': 0.5,'center_y': 0.5},
-                width = 300,
-                size_hint_x = None
-                )
+                        hint_text = "Nhập mã chứng khoán",
+                        helper_text_mode = "on_focus",
+                        helper_text = "Nhập mã chứng khoán vào đây",
+                        pos_hint = {'center_x': 0.5,'center_y': 0.5},
+                        width = 300,
+                        size_hint_x = None
+                        )
+        
+        btn_search = MDRectangleFlatButton(text = 'Tìm kiếm',
+                                  pos_hint = {'center_x': 0.5,'center_y': 0.4},
+                                  on_press = lambda x :self.get_code(screen=screen,code = self.code.text)
+                                  )
+        
+        
         screen.add_widget(btn_search)
         screen.add_widget(self.code)
-        return screen
+        return screen 
     
-    def get_code(self,obj) :
-        print(self.code.text)
+    def get_code(self,screen,code) :
+        code = str(code).upper()
+        path = ('/').join(os.path.dirname(__file__).split("\\"))
+        list_com = pd.read_csv(path+'/data/list_com1.csv')
+        list_com = pd.concat([list_com['ticker'],list_com['thong tin']],axis=1)
+        list_com = pd.DataFrame(list_com).set_index("ticker")
+        if code not in list_com.index :
+            dialog = MDDialog(title = "Lỗi",text = "Không tìm được mã")
+            dialog.open()
+        else :
+            dialog = MDDialog(title = "Success",text = "Tìm thấy mã rồi nè")
+            dialog.open()
+            print(list_com)
+            info = MDLabel(text = str(list_com.at[code,"thong tin"]))
+            screen.clear_widgets()
+            screen.add_widget(FigureCanvasKivyAgg(candleStick()))
+            # screen.add_widget(info)
+
+
+        
+            
 
     
 MyApp().run()
