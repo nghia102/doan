53c5ada586ed8508c5feb336443ce69e69cd7fec
NGHIANGUYENVAN\ADMIN <nghianguyenvan@vnpt.vn>
2023-10-23 17:52:39 +0700

    update model_train

--- D19_DoAn/model/trainmodel.py
+++ D19_DoAn/model/trainmodel.py
@@ -18,26 +18,31 @@ from pickletools import optimize
 
 import plotly.graph_objects as go
 import plotly.express as px
-path = ('/').join(os.path.dirname(__file__).split("\\")[:-1])
-data_result = pd.read_csv(path+'/data/data.csv')
-data_result = data_result.set_index('Date')
-data = data_result['Close']
-data1 = data
-data = data.values.reshape(-1,1)
-train_data = data[:len(data)//2]
-sc = MinMaxScaler(feature_range=(0,1))
-sc_train =sc.fit_transform(data.reshape(-1,1))
-#tao cac gia tri de train
+def create_model(ticker):
+    path = ('/').join(os.path.dirname(__file__).split("\\")[:-1])
+        # data_result.to_json(path+"\data\data_gia\data_"+ticker_stock+".json",orient="records")
+    # data_result = pd.read_json
+    data_result = pd.read_json(path+'/data/data_gia1/data_'+ticker+'.json')
+    data_result = data_result.set_index('Date')
+    data = data_result['Close']
+    data1 = data
+    data = data.values.reshape(-1,1)
+    train_data = data[:len(data)//2]
+    sc = MinMaxScaler(feature_range=(0,1))
+    sc_train =sc.fit_transform(data.reshape(-1,1))
+    #tao cac gia tri de train
 
-x_train,y_train = [],[]
-for i in range(50,len(train_data)) :
-    x_train.append(sc_train[i-50:i,0])
-    y_train.append(sc_train[i])
-x_train = np.array(x_train)
-y_train = np.array(y_train)
-x_train = np.reshape(x_train,(x_train.shape[0],x_train.shape[1],1))
-y_train = np.reshape(y_train,(y_train.shape[0],1))
-def trainmodel() :
+    x_train,y_train = [],[]
+    for i in range(50,len(train_data)) :
+        x_train.append(sc_train[i-50:i,0])
+        y_train.append(sc_train[i])
+    x_train = np.array(x_train)
+    y_train = np.array(y_train)
+    x_train = np.reshape(x_train,(x_train.shape[0],x_train.shape[1],1))
+    y_train = np.reshape(y_train,(y_train.shape[0],1))
+    return [x_train,y_train]
+def trainmodel(listxy,ticker) :
+    x_train,y_train = listxy[0], listxy[1]
     #xay mo hinh
 
     model = Sequential()
@@ -49,21 +54,29 @@ def trainmodel() :
 
     #train and save model
     #train model
-    save_model = 'save_model.hdf5'
+    path = ('/').join(os.path.dirname(__file__).split("\\")[:-1])
+    save_model = path+'/data/model_train/'+'save_model_'+ticker+'.hdf5'
     best_model = ModelCheckpoint(save_model,monitor="loss",verbose=2,save_best_only=True,mode='auto')
     model.fit(x_train,y_train,epochs=100,batch_size=50,verbose=2,callbacks=[best_model])
+from vnstock import stock_historical_data,listing_companies
+list_com = listing_companies()['ticker']
+for i in range(38,len(list_com)) :
+    try :
+        trainmodel(create_model(list_com[i]),list_com[i])
+    except IndexError :
+        pass
+# def compare_train_and_test(y_train) :
+#     sc = MinMaxScaler(feature_range=(0,1))
+#     y_train = sc.inverse_transform(y_train)
+#     final_model = keras.models.load_model('save_model.hdf5')
+#     y_train_predict = final_model.predict(x_train)
+#     y_train_predict = sc.inverse_transform(y_train_predict) # gia du doan
+#     predict_data = pd.DataFrame(data1[50:len(data1)//2])
+#     predict_data['predict'] = y_train_predict
+#     print(predict_data)
 
-def compare_train_and_test(y_train) :
-    y_train = sc.inverse_transform(y_train)
-    final_model = keras.models.load_model('save_model.hdf5')
-    y_train_predict = final_model.predict(x_train)
-    y_train_predict = sc.inverse_transform(y_train_predict) # gia du doan
-    predict_data = pd.DataFrame(data1[50:len(data1)//2])
-    predict_data['predict'] = y_train_predict
-    print(predict_data)
+#     fig = px.line(predict_data, x=predict_data.index, y=["Close","predict"], title='Close data')
+#     fig.show()
 
-    fig = px.line(predict_data, x=predict_data.index, y=["Close","predict"], title='Close data')
-    fig.show()
-
-# trainmodel()
-compare_train_and_test(y_train)
+# # trainmodel()
+# compare_train_and_test(y_train)
